e776b000e7ae4771c6fa80176df233fffa7e3b21
mihai-aupeo <mihai@aupeo.com>
2014-07-30 14:53:30 +0200

    + monkey patch for Yell::Logger to support Logger::<<

---
+++ lib/yell/logger.rb
@@ -0,0 +1,13 @@
+require 'yell/logger'
+
+unless (Yell::Logger.public_instance_method(:<<) rescue nil)
+
+  module Yell
+    class Logger
+      def << (msg)
+        unknown(msg)
+      end
+    end
+  end
+
+end
